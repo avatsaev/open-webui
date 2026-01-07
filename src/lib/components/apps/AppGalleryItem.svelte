3da64c9ec30b7c6f1fbc6717472d35dc28c8568c
Aslan Vatsaev <aslan.vatsaev@edenred.com>
2026-01-07 15:06:14 +0100

    app gallery pin app to sidebar

--- src/lib/components/apps/AppGalleryItem.svelte
+++ src/lib/components/apps/AppGalleryItem.svelte
@@ -4,10 +4,28 @@
 
 	import { getAppIconImageUrl } from '$lib/apis/apps';
 	import Tooltip from '$lib/components/common/Tooltip.svelte';
+	import Eye from '$lib/components/icons/Eye.svelte';
+	import EyeSlash from '$lib/components/icons/EyeSlash.svelte';
+
+	import { settings } from '$lib/stores';
+	import { updateUserSettings } from '$lib/apis/users';
 
 	const i18n = getContext('i18n');
 
 	export let app: any;
+
+	const pinAppHandler = async (appId) => {
+		let pinnedApps = $settings?.pinnedApps ?? [];
+
+		if (pinnedApps.includes(appId)) {
+			pinnedApps = pinnedApps.filter((id) => id !== appId);
+		} else {
+			pinnedApps = [...new Set([...pinnedApps, appId])];
+		}
+
+		settings.set({ ...$settings, pinnedApps: pinnedApps });
+		await updateUserSettings(localStorage.token, { ui: $settings });
+	};
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
@@ -31,6 +49,26 @@
 			<div class="font-semibold line-clamp-1 capitalize text-sm">
 				{app.title}
 			</div>
+		</Tooltip>
+
+		<Tooltip
+			content={($settings?.pinnedApps ?? []).includes(app?.id)
+				? $i18n.t('Hide from Sidebar')
+				: $i18n.t('Keep in Sidebar')}
+		>
+			<button
+				class="shrink-0 p-1.5 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg transition-colors"
+				on:click={(e) => {
+					e.stopPropagation();
+					pinAppHandler(app?.id);
+				}}
+			>
+				{#if ($settings?.pinnedApps ?? []).includes(app?.id)}
+					<EyeSlash className="size-4" />
+				{:else}
+					<Eye className="size-4" />
+				{/if}
+			</button>
 		</Tooltip>
 	</div>
 
